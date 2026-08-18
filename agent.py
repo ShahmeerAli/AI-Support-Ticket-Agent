985ad9dbc9441084478e239e510e4b113a35f139
Shahmeer.Gradle <shahmeer.devel@gmail.com>
2026-08-18 16:30:00 +0500

    added the confidence node

--- agent.py
+++ agent.py
@@ -7,10 +7,12 @@ from langchain.messages import HumanMessage,AIMessage
 from dotenv import load_dotenv
 from typing import Annotated, Optional, TypedDict
 from langchain_community.vectorstores import Chroma
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.tools import tool
 from langchain_community.document_loaders import PyPDFLoader
 from langgraph.graph import START, StateGraph
 from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
@@ -26,16 +28,15 @@ model=ChatHuggingFace(
 )
 
 
-#class to verify the confidence if its less than 8 contact helpline
-class ConfidenceState(TypedDict):
-    confidence:int
-
+#class to verify the confidence if its less than 8 contact helpline and the main node
 class AIBOTState(TypedDict):
     message:str
     ragResponse:str
+    confidence:int
+    humanResponse:bool
     
 
-def RAGNode():
+def RAGNode(state:AIBOTState):
     #Loading the document
     loader=PyPDFLoader(
         file_path="DOC.pdf"
@@ -62,8 +63,64 @@ def RAGNode():
         splitter_docs,embeddings
     )
     #Similarity Search-----
-    retriever_docs=vectorSore.as_retriever(
-        search_type='similarity',kwargs={'k':3}
+    retriever_docs=vectorSore.similarity_search_with_relevance_scores(
+        search_type='similarity',search_kwargs={'k':3}
     )
+
+    documents = [
+        doc for doc, score in retriever_docs
+    ]
+
+    # -----------------------------
+    # Create context
+    context = "\n\n".join(
+        doc.page_content
+        for doc in documents
+    )
+
+    prompt=ChatPromptTemplate.from_template(
+        """
+        Answer the Question only from the provided context.
+        Question: {question}
+        Context:  {context}
+        If the context is not enough you simply say contact helpline.
+        """
+    )
+
+    parser=StrOutputParser()
     
+    chain=prompt | model|parser
+    output=chain.invoke({
+        "context":context,
+        "question":state['message']
+    })
+    return {'ragResponse':output}
+
+
+
+def ConfidenceScore(state:AIBOTState):
+    prompt=ChatPromptTemplate.from_template(
+        """
+        Evaluate the following answer.
+        Question:
+        {question}
+        Answer:
+        {answer}
+        Determine how well the answer is supported by the available
+        information.
+        Return a confidence score from 0 to 10.
+        10 = completely supported
+        8  = strongly supported
+        5  = partially supported
+        0  = unsupported
+
+        Return ONLY the number.
+        """
+    )
+    chain=prompt|model|StrOutputParser()
+    output=chain.invoke({
+        'question':state["message"],
+        'answer':state['ragResponse']
+    })
+    return {'confidence':output}
 
